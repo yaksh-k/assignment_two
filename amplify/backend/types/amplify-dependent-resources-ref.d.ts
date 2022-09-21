3fdc98afc287886cf51409df1077d58874dab5bb
yaksh-k <92572990+yaksh-k@users.noreply.github.com>
2022-09-21 20:31:37 +1200

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "assignmenttwo78bc23f4": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "assignment2resource": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Amplify from "aws-amplify";
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import { Storage } from "aws-amplify";
+
+import awsExports from "./aws-exports";
+import { useState } from "react";
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
+  const [fileData, setFileData] = useState();
+  const [fileStatus, setFileStatus] = useState(false);
+  const s3URi = 's3://compx527assignment2bucket192340-dev/public/';
+
+  var imageURI = "";
+
+  const uploadFile = async () => {
+    const result = await Storage.put(fileData.name, fileData, {
+      contentType: fileData.type,
+    });
+    setFileStatus(true);
+    console.log(21, result);
+  };
+
+  fileStatus ? imageURI = s3URi + fileData.name  : imageURI = "Undefined"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Cat and Dog Classifier</h1>
+        <h2>COMPX527 A2</h2>
+      </div>
+
+      <div>
+        <input type="file" multiple accept=".jpg, .png" onChange={(e) => setFileData(e.target.files[0])} />
+      </div>
+
+      <div>
+        <button onClick={uploadFile}>Upload file</button>
+      </div>
+      
+      <div>
+        {fileStatus ? "Image uploaded successfully." : "Please upload an image."}
+        <br />
+
+        {"Image URI: " + imageURI}
+      </div>
+
+      <div>
+        <button onClick={signOut}>Sign out</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
